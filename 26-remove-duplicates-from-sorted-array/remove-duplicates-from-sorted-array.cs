4133785c89b74c4d2e912644a85a2700aa7897b2
Mahmoud R. Keshk <80346721+MahmoudRKeshk@users.noreply.github.com>
2025-08-19 03:12:53 +0300

    Time: 0 ms (100.00%) | Memory: 51 MB (68.83%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
@@ -1,27 +1,15 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums)
     {
-        var queue = new Queue<int>();
-        int uniqueNumbers = 0;
-        var set = new HashSet<int>();
+        var pointer = 1;
 
-        for (int i = 0; i < nums.Length; i++)
+        for (var i = 1; i < nums.Length; i++)
         {
-            if (set.Add(nums[i]))
-            {
-                uniqueNumbers++;
-                if (queue.Count > 0)
-                {
-                    nums[queue.Dequeue()] = nums[i];
-                    nums[i] = '_';
-                    queue.Enqueue(i);
-                }
-                continue;   
-            }
-            queue.Enqueue(i);
-            nums[i] = '_';
+            if (nums[i] == nums[pointer - 1]) continue;
+            nums[pointer] = nums[i];
+            pointer++;
         }
 
-        return uniqueNumbers;
+        return pointer;
     }
 }
